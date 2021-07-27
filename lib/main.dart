83b736a865d2e6261a4dff9ba06fded221495e88
MUmerYasin <mumeryasin123456789@gmail.com>
2021-07-27 01:01:36 -0700

    Test flutter_spinkit 5.0.0, loading indicators animated with flutter

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+
+            mainAxisAlignment: MainAxisAlignment.start,
+
+            children: <Widget>[
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Flutter Spinkit"),
+
+
+                  ],
+              ),
+
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                   SpinKitCircle(
+                     color: Colors.pink,
+                   ),
+                  SpinKitChasingDots(
+                    color: Colors.pink,
+                    size: 40,
+                  ),
+                  SpinKitPumpingHeart(
+                    color: Colors.redAccent,
+                    size: 50,
+                  ),
+                ],
+              ),
+
+            ],
+          ),
+        ),
+      ),
+     // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
